080bc6b436bd735fc1a12f41892666287bf989b6
Dede Hamzah <dehamzah@gmail.com>
2025-12-17 14:06:21 +0700

    add bundle visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import svelte from '@astrojs/svelte';
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [
+            tailwindcss(),
+            visualizer({
+                emitFile: false,
+                filename: "stats.html",
+            })
+        ],
     },
     integrations: [svelte()],
 });
