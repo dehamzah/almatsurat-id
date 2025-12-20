dfc015d2c0fa5778c3715d81bcfe1e9d915c54c6
Dede Hamzah <dehamzah@gmail.com>
2025-12-20 14:15:25 +0700

    chore: remove legal comments on js dist output

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,9 @@ export default defineConfig({
                 filename: "stats.html",
             }),
         ],
+        esbuild: {
+            legalComments: 'none',
+        },
     },
     integrations: [
         svelte(),
