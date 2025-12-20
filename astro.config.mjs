b36108f477835f6d797c4371492b6cc26bd8ed1b
Dede Hamzah <dehamzah@gmail.com>
2025-12-20 14:51:47 +0700

    fix: build stylesheet inline to avoid flash of unstyled content on page refresh after the assets are cached by service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://almatsurat.id',
+    build: {
+        inlineStylesheets: 'always',
+    },
     vite: {
         plugins: [
             tailwindcss(),
