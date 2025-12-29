faa289a4edffb624c43e93b49d8328b2d42e315b
Hamzah <dehamzah@gmail.com>
2025-12-29 13:18:48 +0700

    fix: fix wrong import syntax in sentry init

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/astro";
 import { version } from "./package.json";
 
 Sentry.init({
-  dsn: process.env.PUBLIC_SENTRY_DSN,
+  dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
     sendDefaultPii: true,
